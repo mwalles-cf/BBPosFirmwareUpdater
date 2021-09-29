68cd93a29a01be1fe299efac8055f6e07da5240a
Micah A. Walles <micah.walles@cardflight.com>
2021-09-29 10:29:19 -0500

    Initial Commit

---
+++ BBPos Firmware Updater/BBPos_Firmware_UpdaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBPos_Firmware_UpdaterApp.swift
+//  BBPos Firmware Updater
+//
+//  Created by Micah A. Walles on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct BBPos_Firmware_UpdaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBPos Firmware Updater/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BBPos Firmware Updater
+//
+//  Created by Micah A. Walles on 9/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
